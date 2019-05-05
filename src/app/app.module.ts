c33d477e218f8c205a91ce49b6ddee11b7a24485
Aleksei Tukhfatullin <alextb1989@gmail.com>
2019-05-05 17:36:11 +0300

    add filter fild for serch film commit2

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,43 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import { AppComponent } from './app.component';
+import { PreviweComponent } from './previwe/previwe.component';
+import { SerchrandomfilmComponent } from './serchrandomfilm/serchrandomfilm.component';
+import { ShowfilmComponent } from './showfilm/showfilm.component';
+
+import { SerchrandomfilmService } from './shared/serchrandomfilm.service';
+import {MatButtonModule, MatCheckboxModule} from '@angular/material';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatSelectModule} from '@angular/material/select';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import {MatIconModule} from '@angular/material/icon';
+import { SelectAutocompleteModule } from 'mat-select-autocomplete';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PreviweComponent,
+    SerchrandomfilmComponent,
+    ShowfilmComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatButtonModule,
+    MatCheckboxModule,
+    MatFormFieldModule,
+    MatSelectModule,
+    FormsModule,
+    ReactiveFormsModule,
+    MatIconModule,
+    SelectAutocompleteModule,
+
+
+
+   
   ],
-  providers: [],
+  providers: [SerchrandomfilmService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
